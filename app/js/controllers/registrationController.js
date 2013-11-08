620b08e8b9d238210cc0c3f5125dc9f81aecbe64
Jon Lunsford <jon@capturethecastle.net>
2013-11-08 14:01:26 -0800

    added registration flow

--- app/js/controllers/registrationController.js
+++ app/js/controllers/registrationController.js
@@ -6,8 +6,26 @@ onTapControllers.controller("registrationController", [
     var baseRegistration = Restangular.all("users")
     
     $scope.register = function(email, password) {
-      var newAccount = user[{email: email, password: password}];
-      baseRegistration.post(newAccount);
+      $scope.emailErrors = false;
+      $scope.passwordErrors = false;
+
+      user = {email: email, password: password};
+      baseRegistration.post({user: user}).then(function(response) {
+        $scope.storeCredentials(response);
+      }, function(response) {
+        if(response.data.email) {
+          $scope.emailErrors = true;
+          $scope.emailValidation = "Email address " + response.data.email[0];
+        } else if(response.data.password) {
+          $scope.passwordErrors = true;
+          $scope.passwordValidation = "Password " + response.data.password[0]
+        }
+      })
+    }
+
+    $scope.storeCredentials = function(serverDate) {
+      var storage = window.localStorage;
+      storage.setItem("auth_token", serverDate.authentication_token);
     }
   }
 ]);
