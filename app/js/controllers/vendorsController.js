326f74578d7aabcc2adec2d605c678b8d1e83558
jonlunsford <jon@capturethecastle.net>
2014-01-20 15:43:24 -0800

    added more details to vendor view

--- app/js/controllers/vendorsController.js
+++ app/js/controllers/vendorsController.js
@@ -5,27 +5,42 @@ onTapControllers.controller("vendorsController", [
   "flash",
   "sessionService",
   "$navigate",
+  "$route",
 
-  function vendorsController($scope, Restangular, storage, flash, sessionService, $navigate) {
+  function vendorsController($scope, Restangular, storage, flash, sessionService, $navigate, $route) {
     $scope.userId = storage.get("user_id");
     $scope.$navigate = $navigate;
-    currentUser = Restangular.one("users", $scope.userId);
-    getRequest = currentUser.get({auth_token: storage.get("auth_token")});
+    $scope.beerFollowersCount = "";
 
+    currentUser = Restangular.one("users", $scope.userId);
+
+    getRequest = currentUser.get({auth_token: storage.get("auth_token")});
     getRequest.then(function(user) {
+      var beerFollowersArr = [];
+
       $scope.user = user;
-      $scope.userName = user.name;
+      $scope.name = user.name;
+      $scope.userName = user.user_name;
       $scope.userEmail = user.email;
       $scope.userDescription = user.description;
       $scope.userEstablished = user.established;
       $scope.userWebsite = user.website;
       $scope.userAddressLineOne = user.address_line_one;
       $scope.userAddressLineTwo = user.address_line_two;
+      $scope.beerCount = user.beers.length;
+      $scope.followerCount = user.follower_relationships.length;
+      
+      _.each(user.beers, function(element) {
+        beerFollowersArr.push(element.user_beer_relationships);
+      });
+
+      $scope.beerFollowersCount = beerFollowersArr.length;
     });
 
     $scope.saveUserInfo = function() {
       var userInfo = {
-        name: $scope.userName,
+        name: $scope.name,
+        user_name: $scope.userName,
         email: $scope.userEmail,
         description: $scope.userDescription,
         established: $scope.userEstablished,
@@ -36,10 +51,21 @@ onTapControllers.controller("vendorsController", [
 
       putRequest = currentUser.customPUT({user: userInfo, auth_token: storage.get("auth_token")});
       putRequest.then(function(user) {
-        flash("Profile updated successfully!");
+        flash("Profile updated successfully");
         $scope.hideFlashes();
       });
     };
+
+    $scope.getBeerFollowers = function() {
+      var getRequest = currentUser.one("beer_followers").get({auth_token: storage.get("auth_token")});
+      getRequest.then(function(users) {
+        $scope.beerFollowers = users;
+      });
+    };
+
+    if($route.current.originalPath.indexOf("beer_followers") > 0) {
+      $scope.getBeerFollowers();
+    }
 
     $scope.signOut = function() {
       sessionService.destroySession();
